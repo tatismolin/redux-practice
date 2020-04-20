6eef53e503d29e94b7022660105113fe8cbfac7c
Tatiana Smolin <tatismolin@gmail.com>
2020-04-20 13:08:23 -0600

    Adds Counter Component and buttons.

--- app.js
+++ app.js
@@ -9,16 +9,39 @@ const counter = (state = 0, action) => {
     };
 };
 
+const Counter = ({
+    value,
+    onIncrement,
+    onDecrement
+}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+);
+
 const { createStore} = Redux;
 const store = createStore(counter);
 
 const render = () => {
-    document.body.innerText = store.getState();
+    ReactDOM.render(
+        <Counter 
+            value={store.getState()} 
+            onIncrement={() => 
+                store.dispatch({
+                    type: "INCREMENT"
+                })
+            }
+            onDecrement={() => 
+                store.dispatch({
+                    type: "DECREMENT"
+                })
+            } 
+        />,
+        document.getElementById("root")
+    );
 };
 
 store.subscribe(render);
 render();
-
-document.addEventListener("click", () => {
-    store.dispatch({type: "INCREMENT"});
-});
